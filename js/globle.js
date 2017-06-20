03d24db7d1d4d6bbe7ae6110fb27f64bff631de2
xnraiser00q <xnraiser00q@gmail.com>
2017-06-20 15:49:54 +0800

    将登陆用js整合到globle.js

---
+++ js/globle.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    //display hidden modal
+    $("#signin").click(function(){
+        $("#signinpage").modal('show');
+    });
+    $("#signup").click(function(){
+        $("#signuppage").modal('show');
+
+    });
+    $("#gotosignup").click(function(){
+        $("#signinpage").modal('hide');
+        $("#signuppage").modal('show');
+    });
+    //
+    $('#signinconfirm').click(function(){
+        if ($('#username').val()=='') {
+            alert("用户名不能为空!");
+            $('#username').focus();
+            return false;
+        }
+        if ($('#password').val()=='') {
+            alert("密码不能为空!");
+            $('#password').focus();
+            return false;
+        }
+    });
+    $('#signupconfirm').click(function(){
+        if ($('#createusername').val()=='') {
+            alert("用户名不能为空!");
+            $('#createusername').focus();
+            return false;
+        }
+        if ($('#createpassword').val()=='') {
+            alert("密码不能为空!");
+            $('#createpassword').focus();
+            return false;
+        }
+    });
+});
